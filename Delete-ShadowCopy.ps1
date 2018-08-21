c4feeafad78627bd057e07c4691b842605aaaf46
Deven Yang <394707312@qq.com>
2018-08-21 14:27:03 +0800

    initial commit

---
+++ Delete-ShadowCopy.ps1
@@ -0,0 +1,15 @@
+﻿Param (
+        [parameter(Mandatory = $true, ValueFromPipeline=$true)]
+        [String]$DriveLetter,
+
+        [parameter(Mandatory = $false)]
+        [int]$DaysToKeep=30
+)
+
+$DeviceID = (Get-WmiObject -Class Win32_Volume |? {$_.DriveLetter -eq $driveletter}).DeviceID
+$copies = Get-WmiObject -Class win32_shadowcopy | ? {$_.VolumeName -eq $DeviceID} |select deviceobject,ID,@{n='datetime';e={[management.managementDateTimeConverter]::ToDateTime($_.installdate)}},@{n='dayofyear';e={[management.managementDateTimeConverter]::ToDateTime($_.installdate).dayofyear}}  |?{$_.datetime -lt (Get-Date).AddDays(-$DaysToDelete)}
+foreach ($copy in $copies)
+{
+    ((Get-WmiObject -Class win32_shadowcopy) |? {$_.ID -eq $copy.id}).delete()
+    Write-host $copy
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Delete-ShadowCopy
 This is the script example working with Windows Shadow Copy in PowerShell
+
+#Example
+
+Delete-ShadowCopy.ps1 -DriveLetter C: -DaysToDelete 1
+
+This will delete shadows on C: older than 1 day(s)
